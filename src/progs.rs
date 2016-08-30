8ada1ab042821ce66b7715d8614bb3d9e9fd995d
Cormac O'Brien <cormac@c-obrien.org>
2016-08-30 01:04:30 -0500

    More opcode implementations and some whitespace :(

--- src/progs.rs
+++ src/progs.rs
@@ -1,14 +1,14 @@
 // Copyright © 2015 Cormac O'Brien.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 // and associated documentation files (the "Software"), to deal in the Software without
 // restriction, including without limitation the rights to use, copy, modify, merge, publish,
 // distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
 // BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -18,8 +18,8 @@
 //! QuakeC bytecode interpreter
 //!
 //! ### Loading from disk
-//! 
-//! 
+//!
+//!
 
 use std::io::{Read, Seek, SeekFrom};
 use std::fs::File;
@@ -76,7 +76,7 @@ struct Progs {
 }
 
 impl Progs {
-    fn load<P>(&self, path: P) -> Progs 
+    fn load<P>(&self, path: P) -> Progs
             where P: AsRef<Path> {
         let mut f = File::open(path).unwrap();
 
@@ -113,11 +113,19 @@ impl Progs {
         //let mut globaldef_vec = Vec::with_capacity(globaldef_lump.count);
         for _ in 0..globaldef_lump.count {
         }
-        
+
         Progs {
             text: Default::default(),
             data: Default::default(),
         }
+    }
+
+    fn load_u(&self, addr: u16) -> u32 {
+        (&self.data[addr as usize..]).load_u32le()
+    }
+
+    fn store_u(&mut self, val: u32, addr: u16) {
+        (&mut self.data[addr as usize..]).write_u32::<LittleEndian>(val);
     }
 
     fn load_f(&self, addr: u16) -> f32 {
@@ -146,57 +154,150 @@ impl Progs {
     }
 
     // ADD_F: Float addition
-    fn add_f(&mut self, term1: u16, term2: u16, sum: u16) {
-        let a = self.load_f(term1);
-        let b = self.load_f(term2);
-        self.store_f(a + b, sum);
+    fn add_f(&mut self, f1_addr: u16, f2_addr: u16, sum_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f1_addr);
+        self.store_f(f1 + f2, sum_addr);
     }
 
     // ADD_V: Vector addition
-    fn add_v(&mut self, term1: u16, term2: u16, sum: u16) {
-        let a = self.load_v(term1);
-        let b = self.load_v(term2);
-        self.store_v(a + b, sum);
+    fn add_v(&mut self, v1_addr: u16, v2_addr: u16, sum_addr: u16) {
+        let v1 = self.load_v(v1_addr);
+        let v2 = self.load_v(v2_addr);
+        self.store_v(v1 + v2, sum_addr);
     }
 
     // SUB_F: Float subtraction
-    fn sub_f(&mut self, term1: u16, term2: u16, diff: u16) {
-        let a = self.load_f(term1);
-        let b = self.load_f(term2);
-        self.store_f(a - b, diff);
+    fn sub_f(&mut self, f1_addr: u16, f2_addr: u16, diff_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(f1 - f2, diff_addr);
     }
 
     // SUB_V: Vector subtraction
-    fn sub_v(&mut self, term1: u16, term2: u16, diff: u16) {
-        let a = self.load_v(term1);
-        let b = self.load_v(term2);
-        self.store_v(a - b, diff);
+    fn sub_v(&mut self, v1_addr: u16, v2_addr: u16, diff_addr: u16) {
+        let v1 = self.load_v(v1_addr);
+        let v2 = self.load_v(v2_addr);
+        self.store_v(v1 - v2, diff_addr);
     }
 
     // MUL_F: Float multiplication
-    fn mul_f(&mut self, term1: u16, term2: u16, prod: u16) {
-        let a = self.load_f(term1);
-        let b = self.load_f(term2);
-        self.store_f(a * b, prod);
+    fn mul_f(&mut self, f1_addr: u16, f2_addr: u16, prod_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(f1 * f2, prod_addr);
     }
 
     // MUL_V: Vector dot-product
-    fn mul_v(&mut self, term1: u16, term2: u16, dot: u16) {
-        let a = self.load_v(term1);
-        let b = self.load_v(term2);
-        self.store_f(a.dot(b), dot);
+    fn mul_v(&mut self, v1_addr: u16, v2_addr: u16, dot_addr: u16) {
+        let v1 = self.load_v(v1_addr);
+        let v2 = self.load_v(v2_addr);
+        self.store_f(v1.dot(v2), dot_addr);
+    }
+
+    // MUL_FV: Component-wise multiplication of vector by scalar
+    fn mul_fv(&mut self, f_addr: u16, v_addr: u16, prod_addr: u16) {
+        let f = self.load_f(f_addr);
+        let v = self.load_v(v_addr);
+        self.store_v(v * f, prod_addr);
+    }
+
+    // MUL_VF: Component-wise multiplication of vector by scalar
+    fn mul_vf(&mut self, v_addr: u16, f_addr: u16, prod_addr: u16) {
+        let v = self.load_v(v_addr);
+        let f = self.load_f(f_addr);
+        self.store_v(v * f, prod_addr);
     }
 
     // DIV: Float division
-    fn div(&mut self, term1: u16, term2: u16, quot: u16) {
-        let a = self.load_f(term1);
-        let b = self.load_f(term2);
-        self.store_f(a / b, quot);
+    fn div_f(&mut self, f1_addr: u16, f2_addr: u16, quot_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(f1 / f2, quot_addr);
     }
-}
 
-fn main() {
-    println!("Hello, world!");
+    // BITAND: Bitwise AND
+    fn bitand(&mut self, u1_addr: u16, u2_addr: u16, and_addr: u16) {
+        let u1 = self.load_u(u1_addr);
+        let u2 = self.load_u(u2_addr);
+        self.store_u(u1 & u2, and_addr);
+    }
+
+    // BITOR: Bitwise OR
+    fn bitor(&mut self, u1_addr: u16, u2_addr: u16, or_addr: u16) {
+        let u1 = self.load_u(u1_addr);
+        let u2 = self.load_u(u2_addr);
+        self.store_u(u1 | u2, or_addr);
+    }
+
+    // GE: Greater than or equal to comparison
+    fn ge(&mut self, f1_addr: u16, f2_addr: u16, ge_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(match f1 >= f2 {
+            true => 1.0,
+            false => 0.0,
+        }, ge_addr);
+    }
+
+    // LE: Less than or equal to comparison
+    fn le(&mut self, f1_addr: u16, f2_addr: u16, le_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(match f1 <= f2 {
+            true => 1.0,
+            false => 0.0,
+        }, le_addr);
+    }
+
+    // GE: Greater than comparison
+    fn gt(&mut self, f1_addr: u16, f2_addr: u16, gt_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(match f1 > f2 {
+            true => 1.0,
+            false => 0.0,
+        }, gt_addr);
+    }
+
+    // LT: Less than comparison
+    fn lt(&mut self, f1_addr: u16, f2_addr: u16, lt_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(match f1 < f2 {
+            true => 1.0,
+            false => 0.0,
+        }, lt_addr);
+    }
+
+    // AND: Logical AND
+    fn and(&mut self, f1_addr: u16, f2_addr: u16, and_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(match f1 != 0.0 && f2 != 0.0 {
+            true => 1.0,
+            false => 0.0,
+        }, and_addr);
+    }
+
+    // OR: Logical OR
+    fn or(&mut self, f1_addr: u16, f2_addr: u16, or_addr: u16) {
+        let f1 = self.load_f(f1_addr);
+        let f2 = self.load_f(f2_addr);
+        self.store_f(match f1 != 0.0 || f2 != 0.0 {
+            true => 1.0,
+            false => 0.0,
+        }, or_addr);
+    }
+
+    // NOT_F: Compare float to 0.0
+    fn not_f(&mut self, f_addr: u16, not_addr: u16) {
+        let f = self.load_f(f_addr);
+        self.store_f(match f == 0.0 {
+            true => 1.0,
+            false => 0.0,
+        }, not_addr);
+    }
 }
 
 #[cfg(test)]
@@ -225,7 +326,7 @@ mod test {
     #[test]
     fn test_progs_store_f() {
         let to_store = 365.0;
-        
+
         let mut progs = Progs {
             data: vec![0, 0, 0, 0].into_boxed_slice(),
             text: Default::default(),
@@ -253,7 +354,7 @@ mod test {
     #[test]
     fn test_progs_store_v() {
         let to_store = Vec3::new(245.2, 50327.99, 0.0002);
-        
+
         let mut progs = Progs {
             data: vec![0; 12].into_boxed_slice(),
             text: Default::default(),
@@ -324,7 +425,7 @@ mod test {
     }
 
     #[test]
-    fn test_progs_div() {
+    fn test_progs_div_f() {
         let f32_size = size_of::<f32>() as u16;
         let term1 = 6.0;
         let t1_addr = 0 * f32_size;
@@ -339,7 +440,7 @@ mod test {
 
         progs.store_f(term1, t1_addr);
         progs.store_f(term2, t2_addr);
-        progs.div(t1_addr as u16, t2_addr as u16, quot_addr as u16);
+        progs.div_f(t1_addr as u16, t2_addr as u16, quot_addr as u16);
         assert!(progs.load_f(quot_addr) == term1 / term2);
     }
 }
